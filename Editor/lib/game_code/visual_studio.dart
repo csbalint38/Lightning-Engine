de12af145fe1b10f0266e2b2e5a33c1011d8ea4a
csbalint38 <82750607+csbalint38@users.noreply.github.com>
2024-08-31 17:38:13 +0200

    vsidll

--- Editor/lib/game_code/visual_studio.dart
+++ Editor/lib/game_code/visual_studio.dart
@@ -1,154 +1,32 @@
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
-import 'package:win32/win32.dart';
+
+typedef _OpenVisualStudioNative = Void Function(Pointer<Utf8> solutionPath);
+typedef _OpenVisualStudio = void Function(Pointer<Utf8> solutionPath);
+
+typedef _CloseVisualStudioNative = Void Function();
+typedef _CloseVisualStudio = void Function();
 
 class VisualStudio {
-  static Pointer<COMObject> _vsInstance = nullptr;
-  static final String _progId = "VisualStudio.DTE.17.0";
-  static final ole32 = DynamicLibrary.open('ole32.dll');
-
-  static final getRunningObjectTable = ole32.lookupFunction<
-      Int32 Function(Uint32 reserved, Pointer<Pointer<COMObject>> pprot),
-      int Function(int reserved,
-          Pointer<Pointer<COMObject>> pprot)>('GetRunningObjectTable');
-  static final createBindCtx = ole32.lookupFunction<
-      Int32 Function(Uint32 reserved, Pointer<Pointer<COMObject>> ppbc),
-      int Function(
-          int reserved, Pointer<Pointer<COMObject>> ppbc)>('CreateBindCtx');
+  // TODO: replace
+  static final _lib = DynamicLibrary.open(
+      "C:/Users/balin/Documents/Lightning-Engine/VisualStudio/bin/x64/Debug/net8.0-windows10.0.22621.0/win-x64/native/vsi.dll");
+  static final _OpenVisualStudio _open_vs_internal =
+      _lib.lookupFunction<_OpenVisualStudioNative, _OpenVisualStudio>(
+          'OpenVisualStudio');
+  static final _CloseVisualStudio _close_vs_internal =
+      _lib.lookupFunction<_CloseVisualStudioNative, _CloseVisualStudio>(
+          'CloseVisualStudio');
 
   static void openVisualStudio(String solutionPath) {
-    final rot = calloc<Pointer<COMObject>>();
-    final monikerTable = calloc<Pointer<COMObject>>();
-    final bindCtx = calloc<Pointer<COMObject>>();
+    final solutionPathPointer = solutionPath.toNativeUtf8();
 
-    int hResult = CoInitializeEx(nullptr, COINIT.COINIT_APARTMENTTHREADED);
-    if (FAILED(hResult)) throw COMException(hResult);
+    _open_vs_internal(solutionPathPointer);
 
-    //try {
-    /*if (_vsInstance == nullptr) {
-      hResult = getRunningObjectTable(0, rot);
-      if (FAILED(hResult))
-        throw Exception(
-            "GetRunningObjectTable() returned HRESULT: ${hResult.toRadixString(16)}");
+    malloc.free(solutionPathPointer);
+  }
 
-      print(rot.address);
-
-      final rotInstance = IRunningObjectTable(rot.cast());
-      print(rotInstance.addRef());
-      hResult = rotInstance.enumRunning(monikerTable);
-      if (FAILED(hResult)) throw COMException(hResult);
-
-      final monikerTableInstance = IEnumMoniker(monikerTable.cast());
-      monikerTableInstance.reset();
-
-      hResult = createBindCtx(0, bindCtx);
-      if (hResult < 0 || bindCtx == nullptr)
-        throw Exception(
-            "CreateBindCtx() returned HRESULT: ${hResult.toRadixString(16)}");
-
-      final bindCtxInstance = IBindCtx(bindCtx.value);
-      final currentMoniker = calloc<Pointer<COMObject>>();*/
-/*
-      while (monikerTableInstance.next(1, currentMoniker, nullptr) == S_OK) {
-        final moniker = IMoniker(currentMoniker.value);
-
-        final displayNamePtr = calloc<Pointer<Utf16>>();
-        moniker.getDisplayName(bindCtxInstance.ptr, nullptr, displayNamePtr);
-
-        final displayName = displayNamePtr.value.toDartString();
-        calloc.free(displayNamePtr);
-
-        if (displayName.contains(_progId)) {
-          final dte2Pointer = calloc<Pointer<COMObject>>();
-          hResult = rotInstance.getObject(moniker.ptr, dte2Pointer);
-          if (hResult < 0)
-            throw Exception(
-                "RunningObjectTable's GetObject() returned HRESULT: ${hResult.toRadixString(16)}");
-
-          _vsInstance = dte2Pointer.value;
-
-          final solutionNamePtr = calloc<Pointer<Utf16>>();
-          final getSolutionName = (dte2Pointer.value.ref.lpVtbl.value + 6)
-              .cast<
-                  Pointer<
-                      NativeFunction<
-                          HRESULT Function(
-                              Pointer, Pointer<Pointer<Utf16>>)>>>()
-              .value
-              .asFunction<int Function(Pointer, Pointer<Pointer<Utf16>>)>();
-
-          getSolutionName(_vsInstance, solutionNamePtr);
-
-          final solutionName = solutionNamePtr.value.toDartString();
-          calloc.free(solutionNamePtr);
-
-          if (solutionName == solutionPath) {
-            break;
-          }
-        }
-
-        moniker.release();
-      }*/
-
-    if (_vsInstance == nullptr) {
-      _vsInstance = calloc<COMObject>();
-      final clsid = calloc<GUID>();
-      final iid = calloc<GUID>();
-
-      hResult = CLSIDFromProgID(TEXT(_progId), clsid);
-      if (FAILED(hResult)) throw COMException(hResult);
-      hResult =
-          IIDFromString(TEXT('{00000000-0000-0000-C000-000000000046}'), iid);
-      if (FAILED(hResult)) throw COMException(hResult);
-
-      hResult = CoCreateInstance(
-          clsid, nullptr, CLSCTX.CLSCTX_LOCAL_SERVER, iid, _vsInstance.cast());
-      print(hResult.toRadixString(16));
-      if (FAILED(hResult)) throw COMException(hResult);
-
-      /////////////////////////////////////////////////////////////////////////
-      //}
-      final dte = IDispatch(_vsInstance.cast());
-      final riid = calloc<GUID>();
-      final nameArray = calloc<Pointer<Utf16>>(1);
-      nameArray[0] = TEXT('MainWindow');
-      final dispId = calloc<Int32>();
-
-      hResult = dte.getIDsOfNames(riid, nameArray, 1, 0, dispId);
-      if (FAILED(hResult)) throw COMException(hResult);
-
-      final pDispParams = calloc<DISPPARAMS>();
-      final pVarResult = calloc<VARIANT>();
-
-      hResult = dte.invoke(
-          dispId.value,
-          riid,
-          0,
-          DISPATCH_FLAGS.DISPATCH_PROPERTYGET,
-          pDispParams,
-          pVarResult,
-          nullptr,
-          nullptr);
-      if (FAILED(hResult)) throw COMException(hResult);
-
-      final mainWindowDispatch =
-          IDispatch(pVarResult.ref.pdispVal as Pointer<COMObject>);
-
-      nameArray[0] = TEXT('Activate');
-      hResult = mainWindowDispatch.getIDsOfNames(riid, nameArray, 1, 0, dispId);
-      if (FAILED(hResult)) throw COMException(hResult);
-
-      hResult = mainWindowDispatch.invoke(
-          dispId.value,
-          riid,
-          0,
-          DISPATCH_FLAGS.DISPATCH_METHOD,
-          pDispParams,
-          nullptr,
-          nullptr,
-          nullptr);
-      if (FAILED(hResult)) throw COMException(hResult);
-//}
-    }
+  static void closeVisualStudio() {
+    _close_vs_internal();
   }
 }
---
+++ VisualStudio/VisualStudio.cs
@@ -0,0 +1,92 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Diagnostics;
+
+namespace LightningEngine.GameCode
+{
+    static class VisualStudio
+    {
+        private static EnvDTE80.DTE2? _vsInstance = null;
+        private static readonly string _progID = "VisualStudio.DTE.17.0";
+
+        [DllImport("ole32.dll")]
+        private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+
+        [DllImport("ole32.dll")]
+        private static extern int GetRunningObjectTable(uint reserved, out IRunningObjectTable pprot);
+
+        [UnmanagedCallersOnly(EntryPoint = "OpenVisualStudio")]
+        public static void OpenVisualStudio(IntPtr solutionStringPointer)
+        {
+            IRunningObjectTable? rot = null;
+            IEnumMoniker? monikerTable = null;
+            IBindCtx? bindCtx = null;
+
+            string solutionPath = Marshal.PtrToStringAnsi(solutionStringPointer)!;
+
+            try
+            {
+                if (_vsInstance == null)
+                {
+                    var hResult = GetRunningObjectTable(0, out rot);
+                    if (hResult < 0 || rot == null) throw new COMException($"GetRunningObjectTable() returned HRESULT: {hResult:X8}");
+
+                    rot.EnumRunning(out monikerTable);
+                    monikerTable.Reset();
+
+                    hResult = CreateBindCtx(0, out bindCtx);
+                    if (hResult < 0 || bindCtx == null) throw new COMException($"CreateBindCtx() returned HRESULT: {hResult:X8}");
+
+                    IMoniker[] currentMoniker = new IMoniker[1];
+
+                    while (monikerTable.Next(1, currentMoniker, IntPtr.Zero) == 0)
+                    {
+                        string name = string.Empty;
+                        currentMoniker[0]?.GetDisplayName(bindCtx, null, out name);
+
+                        if (name.Contains(_progID))
+                        {
+                            hResult = rot.GetObject(currentMoniker[0], out object obj);
+                            if (hResult < 0 || obj == null) throw new COMException($"Running object table's GetObject() returned HRESULT: {hResult:X8}");
+
+                            EnvDTE80.DTE2? dte = obj as EnvDTE80.DTE2;
+                            var solutionName = dte?.Solution.FullName;
+
+                            if (solutionName == solutionPath)
+                            {
+                                _vsInstance = dte;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (_vsInstance == null)
+                    {
+                        Type visualStudioType = Type.GetTypeFromProgID(_progID, true)!;
+                        _vsInstance = Activator.CreateInstance(visualStudioType) as EnvDTE80.DTE2;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            finally {
+                if(monikerTable != null) Marshal.ReleaseComObject(monikerTable);
+                if(rot != null) Marshal.ReleaseComObject(rot);
+                if(bindCtx != null) Marshal.ReleaseComObject(bindCtx);
+            }
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "CloseVisualStudio")]
+        public static void CloseVisualStudio()
+        {
+            if(_vsInstance?.Solution.IsOpen == true)
+            {
+                _vsInstance.ExecuteCommand("File.SaveAll");
+                _vsInstance.Solution.Close();
+            }
+            _vsInstance?.Quit();
+        }
+    }
+}
