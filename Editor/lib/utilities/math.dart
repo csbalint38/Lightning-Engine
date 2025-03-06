31eb8b0172adb7e9fdc1e26f848fd0e88fa2616e
csbalint38 <82750607+csbalint38@users.noreply.github.com>
2025-03-06 20:41:16 +0100

    Cleanup

--- Editor/lib/utilities/math.dart
+++
@@ -1,9 +0,0 @@
-class Math {
-  static const double epsilon = 0.00001;
-
-  static bool isNearEqual(double? value, double? other) {
-    if (value == null || other == null) return false;
-
-    return (value - other).abs() < Math.epsilon;
-  }
-}
