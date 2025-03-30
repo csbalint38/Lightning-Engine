c390c13123a414a8d023b3d545fa1ea80eef48a3
csbalint38 <82750607+csbalint38@users.noreply.github.com>
2025-03-30 21:03:38 +0200

    Geometry Asset classes

---
+++ Editor/Content/Geometry.cs
@@ -0,0 +1,15 @@
+﻿using Editor.Common.Enums;
+using System.Diagnostics;
+
+namespace Editor.Content
+{
+    internal class Geometry : Asset
+    {
+        public Geometry() : base(AssetType.MESH) { }
+
+        internal void FromRawData(byte[] data)
+        {
+            Debug.Assert(data?.Length > 0);
+        }
+    }
+}
