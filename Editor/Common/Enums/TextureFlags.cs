bb63a785615587632a72c8ab7d01f7c7f65f0f68
Bálint Cséfalvay <balint.csefalvay@gmail.com>
2025-04-11 15:28:41 +0200

    Texture import settings

---
+++ Editor/Common/Enums/TextureFlags.cs
@@ -0,0 +1,12 @@
+﻿namespace Editor.Common.Enums
+{
+    public enum TextureFlags : int
+    {
+        IS_HDR = 0x01,
+        HAS_ALPHA = 0x02,
+        IS_PREMULTIPLIED_ALPHA = 0x04,
+        IS_IMPORTED_AS_NORMAL_MAP = 0x08,
+        IS_CUBE_MAP = 0x10,
+        IS_VOLUME_MAP = 0x20,
+    }
+}
---
+++ Editor/Utilities/EnumExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Editor.Utilities
+{
+    static class EnumExtension
+    {
+        public static string GetDescription(this Enum value) =>
+            (
+                value
+                    .GetType()
+                    .GetField(value.ToString())
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[]
+            ).FirstOrDefault()?.Description ?? value.ToString();
+    }
+}
